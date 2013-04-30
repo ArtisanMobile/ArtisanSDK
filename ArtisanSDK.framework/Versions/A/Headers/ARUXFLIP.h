76c7a71f39e812f24b702f80dae85b481d488e1f
Daniel Koch <dan@reckonlabs.org>
2013-04-30 14:19:03 -0400

    Pushing RC5

---
+++ ArtisanSDK.framework/Versions/A/Headers/ARUXFLIP.h
@@ -0,0 +1,16 @@
+//
+//  ARUXFLIP.h
+//
+//  Copyright (c) 2013 Artisan Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <ARUXFLIP/ARManager.h>
+#import <ARUXFLIP/ARTrackingManager.h>
+#import <ARUXFLIP/ARExperimentManager.h>
+#import <ARUXFLIP/ARConfigManager.h>
+
+@interface ARUXFLIP : NSObject
+
+@end
