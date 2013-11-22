c382b66b3307f0303ce87f42b99887baebc06483
artisan-ci <artisan-ci-github@apprenaissance.com>
2013-11-22 14:30:26 -0500

    Updated ArtisanSDK with version 2.0.8.1

---
+++ ArtisanSDK.framework/Versions/Current/Headers/UIView+ArtisanNameTag.h
@@ -0,0 +1,15 @@
+//
+//  UIView+ArtisanNameTag.h
+//  ARUXFLIP
+//
+//  Created by Michael Raber on 10/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ArtisanNameTag)
+
+@property (nonatomic, retain) NSString *artisanNameTag;
+
+@end
